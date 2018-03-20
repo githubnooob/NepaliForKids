4a597ff145e9b8c67e6e9fa9542f670bde10a506
root <root@localhost.localdomain>
2018-03-20 17:08:36 +0545

    Started this shit

---
+++ app/src/main/java/com/example/root/nepaliforkids/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.root.nepaliforkids;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
